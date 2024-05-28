069cdc805ade53fa811d51197e24ae84e26907fe
AdimarSarkis <sarkis170@gmail.com>
2024-05-28 19:19:25 -0300

    layout da tela de login feita

--- beginner/Calendar/src/js/login.js
+++ beginner/Calendar/src/js/login.js
@@ -1,45 +1,50 @@
-/*$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-    var $this = $(this),
-        label = $this.prev('label');
-  
-        if (e.type === 'keyup') {
-              if ($this.val() === '') {
-            label.removeClass('active highlight');
-          } else {
-            label.addClass('active highlight');
-          }
-      } else if (e.type === 'blur') {
-          if( $this.val() === '' ) {
-              label.removeClass('active highlight'); 
-              } else {
-              label.removeClass('highlight');   
-              }   
-      } else if (e.type === 'focus') {
-        
-        if( $this.val() === '' ) {
-              label.removeClass('highlight'); 
-              } 
-        else if( $this.val() !== '' ) {
-              label.addClass('highlight');
-              }
-      }
-  
+const inputs = document.querySelectorAll("input");
+const labels = document.querySelectorAll("label");
+const login = document.querySelector(".login");
+const btnSignin = document.querySelector("#signin");
+const btnSignup = document.querySelector("#signup");
+const detail = document.querySelector(".detail");
+
+const loginFormulario = document.querySelector(".tab-content > div:last-child");
+const cadastroFormulario = document.querySelector(".tab-content > div:first-child");
+
+btnSignin.addEventListener("click", () => {
+  login.style.flexDirection = "row-reverse";
+  loginFormulario.style.display = "block";
+  cadastroFormulario.style.display = "none";
+
+  btnSignin.classList.toggle("active");
+  btnSignup.classList.toggle("active");
+});
+
+btnSignup.addEventListener("click", () => {
+  login.style.flexDirection = "row";
+  loginFormulario.style.display = "none";
+  cadastroFormulario.style.display = "block"
+
+  btnSignin.classList.toggle("active");
+  btnSignup.classList.toggle("active");
+});
+
+for(let i=0; i <6; i++){
+
+  inputs[i].addEventListener("focusin", () => {
+    labels[i].classList.add("active");
   });
   
-  $('.tab a').on('click', function (e) {
-    
-    e.preventDefault();
-    
-    $(this).parent().addClass('active');
-    $(this).parent().siblings().removeClass('active');
-    
-    target = $(this).attr('href');
-  
-    $('.tab-content > div').not(target).hide();
-    
-    $(target).fadeIn(600);
-    
-  });*/
+  inputs[i].addEventListener("focusout", () => {
+    (!!!inputs[i].value) ? 
+    labels[i].classList.remove("active")
+    :
+    false;
+  })
+}
 
-  console.log(window.innerWidth)
+setInterval(() => {
+  const tamanhoTela = window.innerWidth;
+  console.log(tamanhoTela)
+  if(tamanhoTela <= 433){
+    inputs[0].style.width = "205%";
+    labels[0].style.width = "205%";
+  }
+}, 1000);
