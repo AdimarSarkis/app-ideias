c1c3e008d7e75512df113e18f437323116341394
AdimarSarkis <sarkis170@gmail.com>
2024-05-29 23:37:51 -0300

    sistema de login feito

--- beginner/Calendar/src/js/login.js
+++ beginner/Calendar/src/js/login.js
@@ -47,4 +47,78 @@ setInterval(() => {
     inputs[0].style.width = "205%";
     labels[0].style.width = "205%";
   }
-}, 1000);
+}, 10000);
+
+// login e validar
+const btnCadastrar = document.querySelector("#cadastro");
+const btnEntrar = document.querySelector("#entrar")
+
+const cadastrar = (e) => {
+  e.preventDefault();
+  const usuarioEmail = inputs[2].value;
+  const usuarioPassword = inputs[3].value;
+  const usuarioPrimeiroNome = inputs[0].value;
+  const usuarioUltimoNome = inputs[1].value;
+  
+  
+  let listaUser = JSON.parse(localStorage.getItem("listaUser") || "[]");
+  
+  listaUser.forEach(element => {
+    if(usuarioEmail != element.Email){
+      listaUser.push({
+        Email: usuarioEmail,
+        Password: usuarioPassword,
+        PrimeiroNome: usuarioPrimeiroNome,
+        UltimoNome: usuarioUltimoNome
+      });
+
+      localStorage.setItem("listaUser", JSON.stringify(listaUser));
+
+      alert("Usuario cadastrado com sucesso");
+
+      login.style.flexDirection = "row-reverse";
+      loginFormulario.style.display = "block";
+      cadastroFormulario.style.display = "none";
+      btnSignin.classList.toggle("active");
+      btnSignup.classList.toggle("active");
+    }
+  })
+
+  alert("Email já em uso")
+}
+
+
+const logar = (e) => {
+  e.preventDefault();
+  let usuarioEmail = inputs[4].value;
+  let usuarioSenha = inputs[5].value;
+  let listaUser = [];
+  let userValid = {
+    email: "",
+    senha: ""
+  };
+
+  listaUser = JSON.parse(localStorage.getItem("listaUser"))
+
+  listaUser.forEach(element => {
+    if(usuarioEmail == element.Email && usuarioSenha == element.Password){
+      userValid = {
+        email: element.Email,
+        senha: element.Password
+      }
+    }
+  });
+
+  if(usuarioEmail == userValid.email && usuarioSenha == userValid.senha){
+    alert("deu certo");
+  }else{
+    alert("Email e/ou Senha incorretos");
+  }
+
+console.log(userValid)
+  
+}
+
+
+btnCadastrar.addEventListener("click", cadastrar)
+btnEntrar.addEventListener("click", logar)
